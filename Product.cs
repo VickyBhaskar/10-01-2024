6aca2e08395eb28dc882933270209f44cf31f3b7
VickyBhaskar <vicky2400kumar@gmail.com>
2024-01-10 15:12:45 +0530

    Final commit

---
+++ Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductHandling
+{
+    public class Product
+    {
+
+        public String PName{ get; set; }
+        public Double PPrice { get; set; }
+        public string PBrand { get; set; }
+        public DateTime MfgDate { get; set; }
+        public DateTime ExpDate { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ProductHandling;
+List<Product> products = new List<Product>()
+{
+    new Product() { PName ="Mobile1",PPrice=6700.00,PBrand="Samsung",MfgDate = new DateTime(day: 12, month: 08, year:2012), ExpDate = new DateTime(day: 12, month: 11, year:2022)},
+    new Product() { PName ="Mobile2",PPrice=7700.00,PBrand="Apple",MfgDate = new DateTime(day: 11, month: 09, year:2011), ExpDate = new DateTime(day: 22, month: 01, year:2021)},
+    new Product() { PName ="Mobile3",PPrice=5700.00,PBrand="RealMe",MfgDate = new DateTime(day: 10, month: 07, year:2008), ExpDate = new DateTime(day: 20, month: 10, year:2020)},
+    new Product() { PName ="Mobile4",PPrice=3700.00,PBrand="Redmi",MfgDate = new DateTime(day: 22, month: 06, year:2010), ExpDate = new DateTime(day: 10, month: 12, year:2023)},
+};
+Console.WriteLine("PName\t PPrice \t PBrand \t MfgDate \t ExpDate");
+foreach (Product prd in products)
+{
+    Console.Write(prd.PName + "\t \t");
+    Console.Write(prd.PPrice + "\t \t");
+    Console.Write(prd.PBrand + "\t \t");
+    Console.Write(prd.MfgDate.ToLongDateString() + "\t \t");
+    Console.Write(prd.ExpDate.ToLongDateString());
+    Console.WriteLine("\n");
+}
+Console.ReadKey();
